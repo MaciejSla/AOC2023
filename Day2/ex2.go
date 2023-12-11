f543c38731af98bed412a518abc22bbbab443dea
Maciej Sładkiewicz <m.sladkiewicz.633@studms.ug.edu.pl>
2023-12-12 00:41:04 +0100

    Day2 Excercise2 done

---
+++ Day2/ex2.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func countMaxCubesInRound(rounds []string) int {
+	maxCubeMap := map[string]int{
+		"red":   0,
+		"green": 0,
+		"blue":  0,
+	}
+	for _, round := range rounds {
+		cubes := strings.Split(round, ",")
+		for _, cube := range cubes {
+			color := strings.Split(cube, " ")[2]
+			value, _ := strconv.Atoi(strings.Split(cube, " ")[1])
+			if maxCubeMap[color] < value {
+				maxCubeMap[color] = value
+			}
+		}
+	}
+	return maxCubeMap["red"] * maxCubeMap["green"] * maxCubeMap["blue"]
+}
+
+func Ex2(input string) {
+	file, _ := os.Open(input)
+	scanner := bufio.NewScanner(file)
+	var result int
+	for scanner.Scan() {
+		line := scanner.Text()
+		game := strings.Split(line, ":")
+		rounds := strings.Split(game[1], ";")
+		result += countMaxCubesInRound(rounds)
+	}
+	fmt.Println(result)
+
+}
